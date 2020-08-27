1a80a0034469a12c30e8b522491bba9e8f07875f
Yuta123456 <k5418070@stu.chs.nihon-u.ac.jp>
2020-08-27 11:53:58 +0900

    save

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,29 @@
+class RecipesController < ApplicationController
+    def show
+        @recipe = Recipe.find(params[:id])
+    end
+    def new
+        @recipe_form = RecipeForm.new
+    end
+    def create
+        @recipe_form = RecipeForm.new
+        @recipe_form.apply(recipe_form_params)
+    
+        if @recipe_form.valid?
+          recipe = Recipe.new(@recipe_form.to_attributes.merge(user_id: current_user.id))
+          Recipe.transaction do
+            if @recipe_form.image_uploaded?
+              image = Image.create!(@recipe_form.to_image_attributes)
+              recipe.image_id = image.id
+            end
+            recipe.save!
+          end
+          redirect_to recipe, notice: 'Recipe was successfully created.'
+        else
+          render :new
+        end
+    end
+    def recipe_form_params
+        params.require(:recipe).permit(:title, :description, :image, :ingredients_text, :steps_text)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+recipes = []
+%w[
+    日持ちするもやしナムル
+    いろんなレシピに！冷凍しておけるカレー
+    お好み焼き大量！
+].each do |recipe_title|
+    recipes << Recipe.create!(
+        title: recipe_title,
+        price: rand(400..600),
+        cooking_time: rand(10..30),
+        description: "#{recipe_title}の作り方",
+        storage_method: "冷蔵庫",
+    )
+end
